7b8821e7a0394f1cb511f45937ba31a7a5e38ab1
gachagajnr <williampius17@users.noreply.github.com>
2023-02-14 16:11:24 +0300

    welcome page complete

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -2,23 +2,23 @@ import { Link, Head } from "@inertiajs/react";
 
 export default function Layout({ children }) {
     return (
-        <div class='bg-white p-4'>
+        <div class="bg-slate-900 p-4">
             <Head title="Tshuts Wrld" />
             <header class="flex  justify-center pt-2 flex-col gap-4 ">
-                <p class="text-center pt-3 text-2xl">Tshuts Wrld</p>
+                <p class="text-center pt-3 text-2xl text-white" >tSHUTwRLD</p>
                 <header className="flex flex-row justify-center ">
                     <Link
-                        className="mr-1 text-sm text-green-700"
+                        className="mr-1 text-sm text-blue-200"
                         href={route("login")}
                     >
-                        Log in
+                        lOG iN
                     </Link>
 
                     <Link
                         href={route("register")}
-                        className="ml-1 text-sm text-blue-700 dark:text-gray-500 "
+                        className="ml-1 text-sm text-blue-300 dark:text-gray-500 "
                     >
-                        Register
+                        rEGISTER
                     </Link>
                 </header>
             </header>
--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,6 +1,6 @@
 import { Link, Head } from "@inertiajs/react";
 import Layout from "@/Layouts/Layout";
-
+import Image from "@/Images/tshirt.png";
 export default function Welcome(props) {
     let tshirts = [
         {
@@ -47,41 +47,42 @@ export default function Welcome(props) {
             <Head title="Tshuts" />
             <div class="md:container md:mx-auto px-4   ">
                 <div class="flex justify-center flex-col  pt-3 border-t ">
-                    <div class="text-2xl text-start tracking-widest overline ">
-                        wORD sHUTS
+                    <div class="flex-shrink-0">
+                        <img class="h-14 w-14" src={Image} alt="Tshut Wrld" />
                     </div>
-                    <div class="text-end text-4xl text-gray-500 border-r-2 border-blue-400 p-3 ">
+
+                    <div class="text-center text-2xl text-white border-r-2 border-blue-400 p-3  ">
                         your own WORDS on t-Shirts
                     </div>
-                    <div class="tracking-tight text-right text-2xl   leading-tight text-rose-600 border-r-2 border-rose-600 p-3 ">
+                    <div class="tracking-tight text-right text-2xl   leading-tight text-amber-500 border-l-2 border-rose-600 p-3 ">
                         Choose from our pHRASES Collection
                     </div>
-                    <div class="tracking-tight text-center border-l-2 text-3xl text-rose-600 hover:text-green-600 font-light P-2 border-blue-500 border-spacing-2 mt-2">
+                    <div class="tracking-tight text-center border-l-2 text-4xl text-rose-600 hover:text-green-600 font-light P-2 border-blue-500 border-spacing-2 mt-2">
                         cHOOSE any dESIGN from oUR cATALOGUE
                     </div>
-                    <div class="tracking-tight text-start text-3xl   text-gray-500  m-4 border-green-400 ">
-                        talk to uS for cUSTOM dESIGNs
+                    <div class="tracking-tight text-start text-2xl border-r-2   text-white  m-4 border-green-400 ">
+                        talk to uS for cUSTOM sOLUTIONs
                     </div>
-                    <button class="bg-gray-800 text-white p-2  rounded-md w-40 text-center    dark:bg-gray-200 border border-transparent">
+                    <button class="bg-gray-800 text-white p-2  rounded-md w-40 text-center    dark:bg-gray-200 border border-white">
                         talk tO uS
                     </button>
                     <div class="flex justify-end gap-3">
-                        <div class="text-center border-blue-600    text-5xl text-blue-600 mt-2">
-                            are you rEADY?
+                        <div class="text-center     text-5xl text-amber-500 mt-2">
+                            rEADY?
                         </div>
-                        <button class="bg-gray-800 text-white p-2  rounded-md w-40 text-center    dark:bg-blue-200 border border-transparent">
+                        <button class="bg-gray-800 text-white p-2 h-10 rounded-md w-40 text-center  border-2  dark:bg-blue-200  border-white">
                             start dESIGNING...
                         </button>
                     </div>
                 </div>
 
-                <div class="text-end tracking-widest overline p-4 mt-4 text-2xl">
+                <div class="text-end text-white tracking-widest overline p-4 mt-4 text-2xl brightness-100 transition ease-in-out delay-150  hover:-translate-x-1 hover:scale-90 duration-300 ">
                     tODAY's pICKS
                 </div>
-                <div class="border-t">
+                <div class="  mt-5 p-4">
                     <div class="flex flex-wrap justify-center gap-3">
-                        <div class="w-full p-4 md:w-1/2 lg:w-1/4 shadow-md">
-                            <a class="relative block h-96 overflow-hidden rounded">
+                        <div class="w-full p-4 md:w-1/2 lg:w-1/4 rounded-md shadow-md border-2 border-cyan-500">
+                            <a class="relative block h-96 overflow-hidden rounded ">
                                 <img
                                     alt="ecommerce"
                                     class="block h-full w-full object-cover object-center cursor-pointer"
@@ -89,16 +90,16 @@ export default function Welcome(props) {
                                 />
                             </a>
                             <div class="mt-4">
-                                <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">
+                                <h3 class="title-font mb-1 text-xs tracking-widest text-white">
                                     PANGA
                                 </h3>
-                                <h2 class="title-font text-lg font-medium text-gray-900">
+                                <h2 class="title-font text-lg font-medium text-white">
                                     KES 700
                                 </h2>
-                                <p class="mt-1">01/09/2022</p>
+                                <p class="mt-1 text-white">01/09/2022</p>
                             </div>
                         </div>
-                        <div class="w-full p-4 md:w-1/2 lg:w-1/4 shadow-md">
+                        <div class="w-full p-4 md:w-1/2 lg:w-1/4   rounded-md shadow-md border-2 border-cyan-500">
                             <a class="relative block h-96 overflow-hidden rounded">
                                 <img
                                     alt="ecommerce"
@@ -107,16 +108,16 @@ export default function Welcome(props) {
                                 />
                             </a>
                             <div class="mt-4">
-                                <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">
+                                <h3 class="title-font mb-1 text-xs tracking-widest text-white">
                                     ADUI
                                 </h3>
-                                <h2 class="title-font text-lg font-medium text-gray-900">
+                                <h2 class="title-font text-lg font-medium text-white">
                                     KES 700
                                 </h2>
-                                <p class="mt-1">01/09/2022</p>
+                                <p class="mt-1 text-white">01/09/2022</p>
                             </div>
                         </div>
-                        <div class="w-full p-4 md:w-1/2 lg:w-1/4 shadow-md">
+                        <div class="w-full p-4 md:w-1/2 lg:w-1/4 shadow-md rounded-md   border-2 border-cyan-500">
                             <a class="relative block  overflow-hidden rounded h-96">
                                 <img
                                     alt="ecommerce"
@@ -125,53 +126,95 @@ export default function Welcome(props) {
                                 />
                             </a>
                             <div class="mt-4">
-                                <h3 class="title-font mb-1 text-xs tracking-widest text-gray-500">
+                                <h3 class="title-font mb-1 text-xs tracking-widest text-white">
                                     LOVE
                                 </h3>
-                                <h2 class="title-font text-lg font-medium text-gray-900">
+                                <h2 class="title-font text-lg font-medium text-white">
                                     KES 700
                                 </h2>
-                                <p class="mt-1">Available Sizes</p>
-                                <p class="mt-0">XXL XL L M S</p>
+                                <p class="mt-1 text-white">Available Sizes</p>
+                                <p class="mt-0 text-white">XXL XL L M S</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="text-center p-3 mt-4 text-3xl tracking-wider">
+                <div class="text-center p-3 mt-4 font-thin font-mono text-4xl tracking-wider text-cyan-500">
                     <q>eVERYDAY iTS oWN mESSAGE or a lANGUAGE yOU cHOOSE</q>
                 </div>
-                <div class="text-start tracking-widest overline p-4 mt-4 text-2xl">
+                <div class="text-start tracking-widest overline p-4 mt-4 text-2xl text-white">
                     aBOUT t-SHUT Wrld
                 </div>
-                <div class="p-3 text-xl leading-relaxed indent-8 italic font-thin tracking-wide text-center">
+                <div class="p-3 text-xl leading-relaxed text-white indent-8 italic font-thin tracking-wide text-center">
                     we strongly agree to many uses of words , sometimes to
                     inspire , to affirm, to pass a message, as well as to
-                    manifest, we print you words on tshirts to deliver your
-                    message to the world We do our own words which appear as
-                    limited edition where we do a series of phrases for a number
-                    of clients with our logo on it You can also request your own
-                    customized phrase at your own pace
+                    manifest,
+                    <div class="text-md text-blue-400 font thin">
+                        we print your choice of words on tshirts to deliver your
+                        message to the world and by crafted designs,
+                    </div>
+                    <div class="text-md text-blue-200 font thin">
+                        We do our own words which appear as limited edition
+                    </div>
+                    where we do a series of phrases for a number of clients with
+                    our logo on it You can also request your own customized
+                    phrase at your own pace
                 </div>
-                <div class="text-end tracking-widest overline p-4 mt-4 text-2xl">
+                <div class="text-end tracking-widest overline p-4 mt-4 text-2xl text-white">
                     pHRASES cATALOGUE
                 </div>
                 <div class="flex flex-wrap justify-center">
-                    <ul class="list-disc">
-                        <li>Be Kind</li>
-                        <li>Spread Love</li> <li>Be Kind</li> <li>Be Kind</li>
-                        <li>Be Kind</li> <li>Be Kind</li>
-                    </ul>
+                    <div class="grid grid-cols-4 gap-4">
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Kind
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Awesome
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Kings Chase Crowns not clowns
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Kind
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Kind
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Awesome
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Kings Chase Crowns not clowns
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Kind
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Kind
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Awesome
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Kings Chase Crowns not clowns
+                        </div>
+                        <div class="shadow-md text-center hover:text-blue-200 text-white bg-black p-1 border-gray-50 rounded">
+                            Be Kind
+                        </div>
+                    </div>
                 </div>
-                <div class="text-center tracking-widest overline p-4 mt-4 text-2xl">
+                <div class="text-blue-300 text-end p-4 m-4 cursor-pointer hover:text-red-500 hover:scale-y-105 duration-75">
+                    Explore Full Catalogue
+                </div>
+                <div class="text-center tracking-widest overline p-4 mt-4 text-2xl text-white">
                     oUR sERVICE
                 </div>
-                <div class="flex flex-wrap justify-center font-thin text-4xl text-blue-600 text-center">
-                    jUST t-sHIRT bRANDING
+                <div class="flex flex-wrap justify-center font-thin text-4xl p-4 text-blue-600 text-center">
+                    ~jUST t-sHIRT bRANDING~
                 </div>
-                <div class="text-start tracking-widest overline p-4 mt-4 text-2xl">
-                    fIND uS oN 
+                <div class="text-center tracking-widest text-white overline p-4 mt-4 text-2xl">
+                    fIND uS oN
                 </div>
-                <div class="flex justify-start gap-2 p-3">
+                <div class="flex justify-center gap-2 p-3">
                     <div href="" class="text-blue-500">
                         Facebook
                     </div>
@@ -182,8 +225,10 @@ export default function Welcome(props) {
                         Twitter
                     </div>
                 </div>
-
-                <h2 class="text-center">
+                <div class="text-start tracking-widest text-white  p-2 mt-4 text-md overline">
+                    @tSHUTwRLD
+                </div>
+                <h2 class="text-end text-white">
                     {"Copyright © "}{" "}
                     <Link to="/" alt="" style={{ textDecoration: "none" }}>
                         TshutWrld{" "}
