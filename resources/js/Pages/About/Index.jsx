29bd61140632444b1fa6adff60e6b5aeeeb9820f
gachagajnr <williampius17@users.noreply.github.com>
2023-05-13 07:27:52 +0300

    about page edits

--- resources/js/Pages/About/Index.jsx
+++ resources/js/Pages/About/Index.jsx
@@ -6,20 +6,20 @@ export default function Index(props) {
         <Layout>
             <Head title="About" />
 
-            <div class="text-white text-4xl text-center p-5   m-2 hover:-translate-y-2 duration-500">
-                About Crafty Tshirts Store
+            <div class="text-black text-4xl text-center p-5   m-2 hover:-translate-y-2 duration-500">
+                About CraftyTshirtsStore
             </div>
 
             <div class="md:container md:mx-auto px-4   ">
                 <div class="flex flex-row justify-center flex-wrap">
-                    <div class=" text-4xl tracking-tight text-center font-extralight p-2 text-transparent bg-clip-text bg-gradient-to-r from-lime-100 to-lime-600">
-                        TShirts
+                    <div class=" text-4xl tracking-tight text-center font-normal p-2 text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-lime-600">
+                        T-Shirts
                     </div>
-                    <div class=" text-4xl tracking-tight text-center font-extralight p-2 text-transparent bg-clip-text bg-gradient-to-r from-lime-100 to-lime-600">
-                        Custom TShirts
+                    <div class=" text-4xl tracking-tight text-center font-normal p-2 text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-lime-600">
+                        Custom T-Shirts
                     </div>
-                    <div class=" text-4xl tracking-tight text-center font-extralight p-2 text-transparent bg-clip-text bg-gradient-to-r from-lime-100 to-lime-600">
-                        Word TShirts
+                    <div class=" text-4xl tracking-tight text-center font-normal p-2 text-transparent bg-clip-text bg-gradient-to-r from-cyan-600 to-lime-600">
+                        Word T-Shirts
                     </div>
                 </div>
                 <div class=" text-5xl text-center tracking-wide font-thin p-2 text-transparent bg-clip-text bg-gradient-to-r from-red-400 to-red-500">
